f4506eda7dfef39e707e71daa9070e5e3a694d14
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-02-10 20:34:49 -0300

    Creating the controller and route for our session

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express')
 const userRouter = require('./users.routes')
 const noteRouter = require('./notes.routes')
 const tagsRouter = require('./tags.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', userRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/notes', noteRouter)
 routes.use('/tags', tagsRouter)
 
