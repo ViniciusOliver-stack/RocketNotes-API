0ff0232bd6db9f34683090293ff3e92148a71e9d
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-02-20 17:48:51 -0300

    Carregando Imagens

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,27 @@
-const {Router} = require('express')
+const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
 
-const usersRoutes = Router()//Vamos executar o Router
+const upload = multer(uploadConfig.MULTER)
+
+const usersRoutes = Router() //Vamos executar o Router
 
 const UsersController = require('../controllers/UsersController')
 const usersController = new UsersController()
 
-usersRoutes.post("/" ,usersController.create)
-usersRoutes.put("/", ensureAuthenticated, usersController.update)
+usersRoutes.post('/', usersController.create)
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file.filename)
+    response.json()
+  }
+)
 
-//Estou exportando esse arquivo para quem quiser utilizar 
+//Estou exportando esse arquivo para quem quiser utilizar
 module.exports = usersRoutes
